de0dc1207f5947ae54508a981cca794711e267e8
João Antão <jp.antao@campus.fct.unl.pt>
2022-11-09 00:26:11 +0000

    working on waitloop test plots

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -14,7 +14,7 @@ rm ${log_file}
 echo "run,node_type,access_type,waitloop_iter,throughput,cache_misses" | tee -a "${log_file}"
 
 for run in $(seq 1 "${n_runs}"); do
-  for w in $(seq 10 10 500); do
+  for w in $(seq 0 50 500); do
     echo "------ run ${run} > ${log_file} ------"
     ./scripts/benchmark-node.sh 0 "dram" "cache-misses:u" 0 "${w}" "${log_file}" "${run}"
     ./scripts/benchmark-node.sh 3 "pmem" "cache-misses:u" 1 "${w}" "${log_file}" "${run}"
