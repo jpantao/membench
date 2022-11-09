a4ff7b392a92bf16ef3876e7f0a54c69a895d005
João Antão <jp.antao@campus.fct.unl.pt>
2022-11-09 14:10:27 +0000

    cache miss e throughput plots

--- scripts/genplots.py
+++ scripts/genplots.py
@@ -1,28 +1,36 @@
 #!/bin/python3
 
 import argparse
+import os
+
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+def plot_access(df, access_pattern, metric, node_type=None):
+    if node_type is None:
+        df_loc = df[((df['access_type'] == f'{access_pattern}_prefetch') | (df['access_type'] == access_pattern))]
+    else:
+        df_loc = df[((df['access_type'] == f'{access_pattern}_prefetch') | (df['access_type'] == access_pattern)) & (
+                df['node_type'] == node_type)]
+
+    means = df_loc.pivot_table(metric, 'waitloop_iter', ['access_type', 'node_type'], aggfunc='mean')
+    errors = df_loc.pivot_table(metric, 'waitloop_iter', ['access_type', 'node_type'], aggfunc='std')
+    means.plot(style='.-')
+    plt.savefig(f'figs/{access_pattern}_{metric}.jpeg')
 
 
 def main():
     df = pd.read_csv(args.csv_file)
     df = df.drop(columns=['run'], axis=0)
 
-    df_seq = df[(df['access_type'] == 'seq_prefetch') | (df['access_type'] == 'seq') & (df['node_type'] == 'dram')]
-    df_rnd = df[((df['access_type'] == 'rnd_prefetch') | (df['access_type'] == 'rnd')) & (df['node_type'] == 'dram')]
-    df_pgn = df[(df['access_type'] == 'pgn_prefetch') | (df['access_type'] == 'pgn') & (df['node_type'] == 'dram')]
+    patterns = ['seq', 'rnd', 'pgn']
 
-    # group = df_seq_prefetch.groupby(['node_type', 'access_type'], as_index=False)
+    for p in patterns:
+        plot_access(df, p, 'throughput')
+        plot_access(df, p, 'cache_misses')
 
-    means = df_rnd.pivot_table('throughput', 'waitloop_iter', ['access_type', 'node_type'], aggfunc='mean')
-    errors = df_rnd.pivot_table('throughput', 'waitloop_iter', ['access_type', 'node_type'], aggfunc='std')
-    print(means)
-
-    # means.plot(x='waitloop_iter', y='node_type', title='Sequential Access (prefetch)')
-    means.plot()
     plt.show()
-
 
 
 if __name__ == '__main__':
@@ -32,4 +40,5 @@ if __name__ == '__main__':
     parser.add_argument('--output-dir', '-o', dest='out_dir', default='figs', help='plots directory (default=./figs)')
     parser.add_argument('csv_file', action='store', help='CSV input file')
     args = parser.parse_args()
+    os.makedirs('figs', exist_ok=True)
     main()
